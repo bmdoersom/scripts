cc56a10e57b4cac31ced67aa0c74f01bbcf6717c
doersombm <brian.doersom@gmail.com>
2020-02-07 11:43:16 -0600

    Updated the scripts post DEMO with AEC

--- powerScripts/aec/moveDirs.sh
+++ powerScripts/aec/moveDirs.sh
@@ -4,7 +4,6 @@
 ## VARS ##
 ##########
 
-#jobDir="/tmp/Estimating/Quotes/2020-Quotes/20202002_ThompsonFrontPatio/"
 jobDir="$1/"
 origBidDocDir="12.OriginalBidDocuments"
 origBidDirFQP=$jobDir$origBidDocDir
